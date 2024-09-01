bae68d373a0bdb5f48d67f2b37119fe2be462258
Alexoficial01 <00001081377847sp@al.educacao.sp.gov.br>
2024-09-01 15:14:26 -0300

    commit save 01/09 - alteração no footer

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style/footer.css" type="text/css">
+ 
     <title>Footer</title>
     <link rel="shortcut icon" href="multimidia/icon/faviconsalao.jpg " type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
@@ -15,32 +15,25 @@
         /* footer Mobile */
 
 /* tudo do footer */
-* {
+
+footer {
+    background-color: #6C6C6C40;
+    color: #000;
     font-family: 'Inria Serif', serif;
-    margin: 0;
-    padding: 0;
 }
 
 footer li {
-    font-size: 12px;
-  
+    font-size: 12px;   
+}
+
+.titulo-sobre-footer {
+    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
     
 }
 
-footer a {
-    text-decoration: none;
-}
-
 .containerfooter {
-    max-width: 1200px;
-    margin: 0 auto;
-}
-
-footer {
-    background-color: #6C6C6C24;
-    color: #000;
-    padding: 10px 0 0;
-    margin-top: 363px;
+    margin-left: 10%;
+    margin-right: 10%; 
 }
 
 .footer-content {
@@ -50,84 +43,69 @@ footer {
 }
 
 .footer-logo img {
-    padding-bottom: 10px;
-    display: block;
-    margin: 0 auto;
-}
-
-.footer-logo p {
-    width: 300px;
-    color: #000;
-    margin: 0 auto;
-}
-
-.links-rapidos, #contatofooter, #localizacaofooter, #servicosfooter {
-    margin-top: 20px;
-}
-
-.links-rapidos h4, #contatofooter h4, #localizacaofooter h4, #servicosfooter h4 {
-    margin-bottom: 20px;
-}
-
-.links-rapidos li {
-    margin-bottom: 10px;
-    
-}
-.links-rapidos, .icon-porta  ul [
-    list-style-image: url('multimidia/icon/portaiconindex.png');
-]
-
-.links-rapidos a {
-    color: #000;
-    text-align: center;
-    font-size: 18px;
-}
-
-
-#contatofooter li {
-    margin: 15px 0;
-    display: flex;
-    gap: 10px;
-    color: #000;
-    font-weight: 200;
-    justify-content: center;
-}
-
-.copyright {
-    background-color: #ced4da;
-    padding: 10px;
-    text-align: center;
-}
-
-.social-icons a {
-    margin-right: 0.5em;
-    color: #000;
-    text-decoration: none;
-    font-size: 24px;
+    margin-top: 50px;
 }
 
 .social-icons {
     display: flex;
     justify-content: center;
-    margin-top: 10px;
+    gap: 10px;
 }
 
-#hr-servico {
-    border: #000000 solid 1px;
-    width: 185px;
-    margin: 20px auto;
+.social-icons a {
+    color: #000;
+    font-size: 24px;
+    text-decoration: none;
 }
 
-#localizacaofooter {
+.links-rapidos, #contatofooter, #localizacaofooter, #servicosfooter {
+    padding-top: 20px;
+    padding-bottom: 30px;
+}
+
+.links-rapidos ul {
+    list-style-image: url('multimidia/icon/portaiconindex.png');
+}
+
+.links-rapidos a {
+    color: #000;
     text-align: center;
+    font-size: 18px;
+    text-decoration: none;    
+}
+
+#contatofooter {
+    padding-top: 50px;
+}
+
+#contatofooter li {   
+    display: flex;
+    justify-content: center;  
 }
 
 #localizacaofooter li {
     list-style: none;
 }
 
+#texto-p {
+    margin-left:5em;   
+}
 
-@media (min-width: 768px) {
+#texto-p li{
+    text-align: left ;
+    
+}
+
+/* direitos autorais */
+
+.copyright {
+    background-color: #696969;
+    padding: 10px;
+    text-align: center;
+    color: #fff;
+}
+
+@media (min-width: 780px) {
     /* footer */
 
     .footer-content {
@@ -135,52 +113,19 @@ footer {
         flex-direction: row;
     }
 
-    footer {
-        padding: 20px 0 0;
-    }
-
-    #servicosfooter hr {
-        margin-top: 10px;
-    }
-
-    #contatofooter {
-        justify-content: start;
-    }
-
-    .footer-logo {
-        border-right: 1px solid #6C6C6C24;
-    }
-
     .links-rapidos, #contatofooter, #localizacaofooter, #servicosfooter {
-        margin-top: 20px;
+        padding-top: 40px;
+        padding-bottom: 50px;
     }
-    
-    .links-rapidos h4, #contatofooter h4, #localizacaofooter h4, #servicosfooter h4 {
-        margin-bottom: 20px;
+
+    #texto-p {
+        width: 65%;
+        margin-left: 4em;    
     }
-    
-    .links-rapidos li {
-        margin-bottom: 10px;
+    .titulo-sobre-footer {
+        font-size: 1vw;
     }
-    
-    .links-rapidos a {
-        color: #000;
-    }
-    
-    #contatofooter li {
-        margin: 0 auto;
-        display: flex;
-        gap: 10px;
-        color: #000;
-        font-weight: 200;
-        justify-content: center;
-    }
-    
-    .copyright {
-        background-color: #ced4da;
-        padding: 10px;
-        text-align: center;
-    }
+
     /* fecha footer */
 }
     </style>
@@ -193,10 +138,12 @@ footer {
             <div class="footer-content">
                 <div class="footer-logo">
                     <picture>
-                        <source media="(min-width: 620px)" srcset="multimidia/logosalaofooter.png">
+                        <source media="(min-width: 780px)" srcset="multimidia/logosalaofooter.png">
                         <img src="multimidia/mobile/logosalaofootermobile.png" alt="logo da barbearia no rodapé">
                     </picture>
+
                     <h3>Salão Novo Estilo</h3>
+                    
                     <div class="social-icons mt-3">
                         <a href="#" target="_blank" class="fab fa-whatsapp"></a>
                         <a href="#" target="_blank" class="fab fa-instagram"></a>
@@ -205,7 +152,7 @@ footer {
 
                 <!-- Contato -->
                 <div  id="contatofooter">
-                    <h4>Contato</h4>
+                    <h4 class="titulo-sobre-footer">Contato</h4>
                     <hr>
                     <ul class="list-unstyled">
                         <li>Email: joseeucledes@hotmail.com</li>
@@ -215,14 +162,14 @@ footer {
 
                  <!-- Localização -->
                  <div  id="localizacaofooter">
-                    <h4>Localização</h4>
+                    <h4 class="titulo-sobre-footer">Localização</h4>
                     <hr>
                     <li>Rua Pedro Baston 176 Itapira - SP</li>
                 </div>
 
 
                 <div id="servicosfooter">
-                    <h4>Serviços</h4>
+                    <h4 class="titulo-sobre-footer">Serviços</h4>
                     <hr>
                     <div id="texto-p">
                         <li>Limpeza de pele</li>
@@ -238,7 +185,8 @@ footer {
                 </div>
 
                 <div class="links-rapidos">
-                    <h4>Links rápidos</h4>
+                    <h4 class="titulo-sobre-footer">Links rápidos</h4>
+                    <hr>
                     <ul class="icon-porta">
                         <li><a href="index.php">Home</a></li>
                         <li><a href="servicos.php">Serviços</a></li>
@@ -253,6 +201,10 @@ footer {
     </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-7J9dD7nE4aR8ae5TW5PH4mF2N5H9Rp/zF3Y3wU5xj5eIz+5mM9oKNhA70Z7PfFNOe" crossorigin="anonymous"></script>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 
 </html>
