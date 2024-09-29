ef2d32d13fefd5729b07af2ff0b008ffe854fe06
Alexoficial01 <00001081377847sp@al.educacao.sp.gov.br>
2024-09-29 18:26:37 -0300

    29/09 banco de dados concluido

--- secure_login_and_sql/sql/script.sql
+++ secure_login_and_sql/sql/script.sql
@@ -1,116 +1,134 @@
 -- SQLBook: Code
-CREATE DATABASE dbSalaoNovoEstilo;
+CREATE DATABASE dbNovoEstilo;
 
-CREATE TABLE dbSalaoNovoEstilo.Clientes (
-    id_cli INT(12) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    nome_cli VARCHAR(50) NOT NULL,
-    tel_cli VARCHAR(20) NOT NULL,
-    data_nasc_cli DATE,
-    numero VARCHAR(10),
-    rua VARCHAR(100),
-    bairro VARCHAR(50),
-    cep VARCHAR(9) -- valor que suporta o hífen, Ex: XXXX-XXX
+-- Tabela Cliente
+CREATE TABLE dbNovoEstilo.Cliente (
+    id_cli INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    data_cadastro_cli DATETIME,
+    nome_cli VARCHAR(100) NOT NULL,
+    nome_social VARCHAR(100),
+    email_cli VARCHAR(100) NOT NULL,
+    telefone_cli VARCHAR(20),
+    celular_cli VARCHAR(20),
+    data_nascimento DATE,
+    tipo_do_documento_cli VARCHAR(50) NOT NULL,
+    documento_cli VARCHAR(50) NOT NULL,
+    uf VARCHAR(2) NOT NULL,
+    cidade VARCHAR(100) NOT NULL,
+    bairro VARCHAR(100) NOT NULL,
+    rua VARCHAR(100) NOT NULL,
+    numero VARCHAR(10) NOT NULL,
+    complemento VARCHAR(100),
+    cep VARCHAR(10),
+    status_cli VARCHAR(20) NOT NULL
 );
 
-CREATE TABLE dbSalaoNovoEstilo.Servicos (
-    id_serv INT(12) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    nome_serv VARCHAR(50) NOT NULL,
-    descricao_serv VARCHAR(255),
-    preco_serv DECIMAL(10, 2) NOT NULL
+-- Tabela Usuario
+CREATE TABLE dbNovoEstilo.Usuario (
+    id_usu INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    nome_usu VARCHAR(100) NOT NULL,
+    email_usu VARCHAR(100) NOT NULL,
+    senha VARCHAR(255) NOT NULL,
+    CPF VARCHAR(14) NOT NULL
 );
 
-/*CREATE TABLE Ordem_De_Servico (
-    id_serv_ped INT(12) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    id_cli INT(12) NOT NULL,
-    id_serv INT(12) NOT NULL,
-    valor_serv DECIMAL(10, 2) NOT NULL,
-    data_serv_ped DATE,
-    CONSTRAINT fk_cliente FOREIGN KEY (id_cli) REFERENCES Clientes(id_cli), /* Constraint -- nomeando as cchaves estrangeiras para futuras alterações 
-    CONSTRAINT fk_servico FOREIGN KEY (id_serv) REFERENCES Servicos(id_serv)
-);*/
-
-CREATE TABLE dbSalaoNovoEstilo.Produtos (
-    id_prod INT(12) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    nome_prod VARCHAR(50) NOT NULL,
-    descricao_prod VARCHAR(255),
-    quant_prod INT NOT NULL, 
-    marca VARCHAR(50) NOT NULL,
-    validade_prod DATE NOT NULL ,
-    preco_prod DECIMAL(10, 2) NOT NULL,
-    status_prod VARCHAR(10) NOT NULL
-);  
-
-/*CREATE TABLE Ped_Prod (
-    id_ped_prod INT(12) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    id_cli INT(12) NOT NULL,
-    id_prod INT(12) NOT NULL,
-    quant_prod_ped INT NOT NULL, 
-    data_ped_prod DATE,
-    valor_prod DECIMAL(10, 2) NOT NULL,
-    CONSTRAINT fk_cliente FOREIGN KEY (id_cli) REFERENCES Clientes(id_cli), 
-    CONSTRAINT fk_produto FOREIGN KEY (id_prod) REFERENCES Produtos(id_prod)
-);*/
-
-CREATE TABLE dbSalaoNovoEstilo.Fornecedores (
-    id_fornecedor INT(12) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    nome_fornecedor VARCHAR(50) NOT NULL,
-    razao_social VARCHAR(255) NOT NULL,
-    email_forn VARCHAR(100) UNIQUE,
-    tel_forn VARCHAR(20),
-    cnpj VARCHAR(14) NOT NULL,
-    numero VARCHAR(10),
-    rua VARCHAR(100),
-    bairro VARCHAR(50),
-    cep VARCHAR(9),
-    uf VARCHAR(2),
-    pais VARCHAR(50),
-    status_fornecedor VARCHAR(10) NOT NULL
+-- Tabela Fornecedor
+CREATE TABLE dbNovoEstilo.Fornecedor (
+    id_for INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    data_cadastro_for DATETIME,
+    nome_for VARCHAR(100) NOT NULL,
+    email_for VARCHAR(100) NOT NULL,
+    telefone_for VARCHAR(20),
+    celular_for VARCHAR(20),
+    tipo_do_documento_for VARCHAR(50) NOT NULL,
+    documento_for VARCHAR(50) NOT NULL,
+    uf VARCHAR(2) NOT NULL,
+    cidade VARCHAR(100) NOT NULL,
+    bairro VARCHAR(100) NOT NULL,
+    rua VARCHAR(100 NOT NULL),
+    cep VARCHAR(10) NOT NULL,
+    status_for VARCHAR(20) NOT NULL
 );
 
-/*CREATE TABLE Itens_Fornecidos (
-    id_prod_fornecido INT(12) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    id_fornecedor INT(12) NOT NULL,
-    id_prod INT(12) NOT NULL,
-    quant_prod_fornecido INT NOT NULL, 
-    valor_prod_fornecido DECIMAL(10, 2) NOT NULL,
-    data_prod_fornecido DATE, 
-    CONSTRAINT fk_fornecedor FOREIGN KEY (id_fornecedor) REFERENCES Fornecedores(id_fornecedor),
-    CONSTRAINT fk_produto FOREIGN KEY (id_prod) REFERENCES Produtos(id_prod)
-);*/
-
-                                                        -- Outra opção 
-/*
-
-    CREATE TABLE Ped_Compra (
-        id_prod_fornecido INT(12) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        id_fornecedor INT(12) NOT NULL,
-        data_prod_fornecido DATE, 
-        CONSTRAINT fk_fornecedor FOREIGN KEY (id_fornecedor) REFERENCES Fornecedores(id_fornecedor)
-    );
-
-
-    CREATE TABLE Itens_Fornecidos (
-        id_prod_fornecido INT(12) NOT NULL,
-        id_prod INT(12) NOT NULL,
-        quant_prod_fornecido INT NOT NULL, 
-        valor_prod_fornecido DECIMAL(10, 2) NOT NULL,
-
-        CONSTRAINT fk_prod_fornecido FOREIGN KEY (id_prod_fornecido) REFERENCES Ped_Compra(id_prod_fornecido),
-        CONSTRAINT fk_produto FOREIGN KEY (id_prod) REFERENCES Produtos(id_prod)
-    );
-
-*/
-
-CREATE TABLE dbSalaoNovoEstilo.Usuarios (
-    id_user INT(12) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    nome_user VARCHAR(50) NOT NULL,
-    email_user VARCHAR(100) NOT NULL UNIQUE, 
-    senha_user VARCHAR(150) NOT NULL,
-    cpf_user VARCHAR(14) NOT NULL UNIQUE
+-- Tabela Produto
+CREATE TABLE dbNovoEstilo.Produto (
+    id_prod INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    nome_prod VARCHAR(100) NOT NULL,
+    desc_prod VARCHAR(255),
+    marca VARCHAR(100) NOT NULL,
+    preco_compra DECIMAL(10,2) NOT NULL,
+    preco_venda DECIMAL(10,2) NOT NULL,
+    estoque_minimo INT NOT NULL,
+    status_prod VARCHAR(20) NOT NULL
 );
 
-select * from dbSalaoNovoEstilo.Clientes;
+-- Tabela Servico
+CREATE TABLE dbNovoEstilo.Servico (
+    id_serv INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    nome_serv VARCHAR(100) NOT NULL,
+    desc_serv VARCHAR(255),
+    prazo_serv DATETIME NOT NULL,
+    preco_serv DECIMAL(10,2),
+    status_serv VARCHAR(20) NOT NULL
+);
 
-select * from dbSalaoNovoEstilo.Produtos;
+-- Tabela items_os
+CREATE TABLE dbNovoEstilo.Items_os (
+    id_ordem INT,
+    id_serv INT,
+    preco_items_os DECIMAL(10,2) NOT NULL,
+    -- FOREIGN KEY (id_serv) REFERENCES Servico(id_serv)
+    -- FOREIGN KEY (id_serv) REFERENCES Servico(id_serv)
+);
 
-select * from dbSalaoNovoEstilo.Fornecedores;
+-- Tabela ordem_servico
+CREATE TABLE dbNovoEstilo.Ordem_servico (
+    id_ordem INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    id_cli INT,
+    id_usu INT,
+    data_ordem_servico DATETIME NOT NULL,
+    -- FOREIGN KEY (id_cli) REFERENCES Cliente(id_cli),
+    -- FOREIGN KEY (id_usu) REFERENCES Usuario(id_usu)
+);
+
+-- Tabela compra
+CREATE TABLE dbNovoEstilo.Compra (
+    id_compra INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    data_compra DATETIME NOT NULL,
+    id_for INT,
+    id_usu INT,
+    prev_entrega DATETIME NOT NULL,
+    data_entrega_efetiva DATETIME NOT NULL,
+    -- FOREIGN KEY (id_for) REFERENCES Fornecedor(id_for),
+    -- FOREIGN KEY (id_usu) REFERENCES Usuario(id_usu)
+);
+
+-- Tabela items_compra
+CREATE TABLE dbNovoEstilo.Items_compra (
+    id_compra INT,
+    id_prod INT,
+    preco_items_compra DECIMAL(10,2) NOT NULL,
+    -- FOREIGN KEY (id_compra) REFERENCES compra(id_compra),
+    -- FOREIGN KEY (id_prod) REFERENCES Produto(id_prod)
+);
+
+-- Tabela pedidos
+CREATE TABLE dbNovoEstilo.Pedido (
+    id_ped INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    data_ped DATETIME NOT NULL,
+    id_cli INT,
+    id_usu INT,
+    endereco_entrega VARCHAR(255) NOT NULL,
+    data_entrega_ped DATETIME NOT NULL,
+    -- FOREIGN KEY (id_cli) REFERENCES Cliente(id_cli),
+    -- FOREIGN KEY (id_usu) REFERENCES Usuario(id_usu)
+);
+
+-- Tabela items_pedido
+CREATE TABLE dbNovoEstilo.items_pedido (
+    id_ped INT,
+    id_prod INT,
+    preco_vendido DECIMAL(10,2) NOT NULL,
+    -- FOREIGN KEY (id_ped) REFERENCES pedidos(id_ped),
+    -- FOREIGN KEY (id_prod) REFERENCES Produto(id_prod)
+);
