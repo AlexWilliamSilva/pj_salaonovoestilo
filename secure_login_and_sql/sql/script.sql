bf2465a02dbbeceb64d57c55dbdaae926d37cd58
alex <alexwilliamgomessilva168@gmail.com>
2024-10-01 17:31:45 -0300

    01/09 - alterando o script sql

--- secure_login_and_sql/sql/script.sql
+++ secure_login_and_sql/sql/script.sql
@@ -26,10 +26,24 @@ CREATE TABLE dbNovoEstilo.Cliente (
 -- Tabela Usuario
 CREATE TABLE dbNovoEstilo.Usuario (
     id_usu INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    data_cadastro_usu DATETIME,
     nome_usu VARCHAR(100) NOT NULL,
+    nome_social VARCHAR(100),
     email_usu VARCHAR(100) NOT NULL,
-    senha VARCHAR(255) NOT NULL,
-    CPF VARCHAR(14) NOT NULL
+    telefone_usu VARCHAR(20),
+    celular_usu VARCHAR(20),
+    data_nascimento DATE,
+    tipo_do_documento_usu VARCHAR(50) NOT NULL,
+    documento_usu VARCHAR(50) NOT NULL,
+    uf VARCHAR(2) NOT NULL,
+    cidade VARCHAR(100) NOT NULL,
+    bairro VARCHAR(100) NOT NULL,
+    rua VARCHAR(100) NOT NULL,
+    numero VARCHAR(10) NOT NULL,
+    complemento VARCHAR(100),
+    cep VARCHAR(10),
+    status_usu VARCHAR(20) NOT NULL,
+    senha VARCHAR(255) NOT NULL
 );
 
 -- Tabela Fornecedor
@@ -56,7 +70,6 @@ CREATE TABLE dbNovoEstilo.Produto (
     nome_prod VARCHAR(100) NOT NULL,
     desc_prod VARCHAR(255),
     marca VARCHAR(100) NOT NULL,
-    preco_compra DECIMAL(10,2) NOT NULL,
     preco_venda DECIMAL(10,2) NOT NULL,
     estoque_minimo INT NOT NULL,
     status_prod VARCHAR(20) NOT NULL
@@ -76,7 +89,7 @@ CREATE TABLE dbNovoEstilo.Servico (
 CREATE TABLE dbNovoEstilo.Items_os (
     id_ordem INT,
     id_serv INT,
-    preco_items_os DECIMAL(10,2) NOT NULL,
+    preco_items_os DECIMAL(10,2) NOT NULL
     -- FOREIGN KEY (id_serv) REFERENCES Servico(id_serv)
     -- FOREIGN KEY (id_serv) REFERENCES Servico(id_serv)
 );
@@ -86,7 +99,7 @@ CREATE TABLE dbNovoEstilo.Ordem_servico (
     id_ordem INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     id_cli INT,
     id_usu INT,
-    data_ordem_servico DATETIME NOT NULL,
+    data_ordem_servico DATETIME NOT NULL
     -- FOREIGN KEY (id_cli) REFERENCES Cliente(id_cli),
     -- FOREIGN KEY (id_usu) REFERENCES Usuario(id_usu)
 );
@@ -99,6 +112,7 @@ CREATE TABLE dbNovoEstilo.Compra (
     id_usu INT,
     prev_entrega DATETIME NOT NULL,
     data_entrega_efetiva DATETIME NOT NULL,
+    preco_compra DECIMAL(10,2) NOT NULL
     -- FOREIGN KEY (id_for) REFERENCES Fornecedor(id_for),
     -- FOREIGN KEY (id_usu) REFERENCES Usuario(id_usu)
 );
@@ -107,19 +121,19 @@ CREATE TABLE dbNovoEstilo.Compra (
 CREATE TABLE dbNovoEstilo.Items_compra (
     id_compra INT,
     id_prod INT,
-    preco_items_compra DECIMAL(10,2) NOT NULL,
+    preco_items_compra DECIMAL(10,2) NOT NULL
     -- FOREIGN KEY (id_compra) REFERENCES compra(id_compra),
     -- FOREIGN KEY (id_prod) REFERENCES Produto(id_prod)
 );
 
--- Tabela pedidos
+-- Tabela pedido
 CREATE TABLE dbNovoEstilo.Pedido (
     id_ped INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     data_ped DATETIME NOT NULL,
     id_cli INT,
     id_usu INT,
     endereco_entrega VARCHAR(255) NOT NULL,
-    data_entrega_ped DATETIME NOT NULL,
+    data_entrega_ped DATETIME NOT NULL
     -- FOREIGN KEY (id_cli) REFERENCES Cliente(id_cli),
     -- FOREIGN KEY (id_usu) REFERENCES Usuario(id_usu)
 );
@@ -128,7 +142,7 @@ CREATE TABLE dbNovoEstilo.Pedido (
 CREATE TABLE dbNovoEstilo.items_pedido (
     id_ped INT,
     id_prod INT,
-    preco_vendido DECIMAL(10,2) NOT NULL,
+    preco_vendido DECIMAL(10,2) NOT NULL
     -- FOREIGN KEY (id_ped) REFERENCES pedidos(id_ped),
     -- FOREIGN KEY (id_prod) REFERENCES Produto(id_prod)
 );
